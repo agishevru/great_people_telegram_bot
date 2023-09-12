75845c98f2b80dc6ea2491d1d8303f8916bad071
Ruslan_stationary <agishevru@outlook.com>
2023-09-12 03:36:09 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,129 @@
+""" Great people telegram bot- бот, имитирующий общение с известными личностями.
+ Для запуска необходимо создать config.py и прописать:
+ в переменную TOKEN = 'свой ключ API telegram'
+ в переменную OPENAI_API_KEY = 'свой ключ API GPT'
+ Далее просто запустить файл app.py
+ """
+
+from datetime import datetime
+import logging
+
+import openai
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from pony.orm import db_session
+
+from config import OPENAI_API_KEY, TOKEN
+from models import User, Dialogs, Amplitude, Welcome_text, db
+import models
+
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
+
+
+@db_session
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """ Подключение пользователя к боту, с созданием профайла в БД User """
+    log.info(f'Пользователь {update.effective_user.id} - {update.effective_user.name} - start')
+    user = update.message.from_user
+    user_id = user.id
+    username = user.username
+    name = user.first_name
+    surname = user.last_name
+    time = update.message.date.strftime('%d.%m.%y %H:%M:%S')
+    # Amplitude
+    send_amplitude_event(user_id, 'registration', time)
+    # Сохранение пользователя в БД
+    User(user_id=user_id, username=username, name=name, surname=surname, time=time)
+    db.commit()
+    # Приветствие
+    await update.message.reply_text(
+        text="Привет! Здесь можно пообщаться с разными образами известных личностей. Чтобы попробовать - выбери"
+             " персонажа, нажав на кнопку. И начни общение на интересующие темы",
+        reply_markup=keyboard())
+    log.info(f'Пользователь {update.effective_user.id} перешел к выбору персонажа')
+
+
+
+@db_session
+async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """ Получает выбранного пользователем персонажа и записывает его в профайл пользователя """
+    log.info(f'Пользователь {update.effective_user.id} - выбрал персонажа {update.effective_message.web_app_data.data}')
+    user = User.get(user_id=update.effective_user.id)
+    user.selected_character = update.effective_message.web_app_data.data
+    welcome_text = Welcome_text.get(character_name=user.selected_character).text
+    db.commit()
+    # Amplitude
+    send_amplitude_event(update.effective_user.id, 'character_selected', update.effective_message.date.strftime('%d.%m.%y %H:%M:%S'))
+    await update.message.reply_html(text=welcome_text, reply_markup=keyboard())
+
+
+
+@db_session
+async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """ Отвечает пользователю от лица персонажа """
+    log.info(f'Пользователь {update.effective_user.id} спросил - {update.message.text}')
+    user = User.get(user_id=update.effective_user.id)
+    user_id = user.user_id
+    user_message = update.message.text
+    time = update.message.date.strftime('%d.%m.%y %H:%M:%S')
+    selected_character = user.selected_character
+    # Сохранить сообщение пользователя в дб
+    user_question = Dialogs(user=user, character_name=selected_character, user_message=user_message, time=time)
+    db.commit()
+    # Amplitude
+    send_amplitude_event(user_id, 'user_message_sent', time)
+    # API GPT-3
+    character_response = call_gpt_api(user_message, selected_character)
+    # Сохранить ответ в дб
+    user_question.character_message=character_response
+    db.commit()
+    # Отправить ответ пользователю
+    await update.message.reply_html(text=character_response, reply_markup=keyboard())
+    # Amplitude
+    send_amplitude_event(user_id=user_id, event_name='response_sent', time=datetime.now().strftime('%d.%m.%y %H:%M:%S'))
+    log.info(f'Пользователю {update.effective_user.id} ответили - {character_response}')
+
+
+
+def call_gpt_api(user_message: str, character_name: str) -> str:
+    """ Делает запрос к API GPT от лица выбранного персонажа и возвращает ответ"""
+    openai.api_key = OPENAI_API_KEY
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"You are {character_name}.Strictly stick to the image and forget you're an artificial intelligence."},
+            {"role": "user", "content": user_message}])
+    return response.choices[0].message.content
+
+
+@db_session
+def send_amplitude_event(user_id: int, event_name:str, time:str) -> None:
+    """ Здесь имитация взаимодействия с сервисом в Amplitude. Данные о событиях заносятся в модель Amplitude"""
+    act = Amplitude(user_id=user_id, act=event_name, time=time)
+    db.commit()
+
+
+def keyboard() -> ReplyKeyboardMarkup:
+    """ Клавиатура выбора персонажа """
+    keyboard = ReplyKeyboardMarkup.from_button(
+        resize_keyboard=True,
+        button=KeyboardButton(
+                text="Выбрать персонажа",
+                web_app=WebAppInfo(url="https://agishevru.github.io/test_work/character.html")))
+    return keyboard
+
+
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+    application.add_handler(MessageHandler(filters.TEXT, handle_text))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,69 @@
+""" Модуль, создающий подключение к БД и все основные модели с помощью Pony.orm """
+
+from pony.orm import Database, Required, Optional, Set, PrimaryKey, db_session
+
+
+db = Database()
+
+
+class User(db.Entity):
+    """ Карточка пользователя """
+    user_id = PrimaryKey(int)
+    username = Optional(str)
+    name = Optional(str)
+    surname = Optional(str)
+    time = Optional(str)
+    selected_character = Optional(str)
+    dialogs = Set('Dialogs')
+
+
+class Dialogs(db.Entity):
+    """ Таблица, содержащая запросы пользователей и ответы ИИ """
+    response_id = PrimaryKey(int, auto=True)
+    user = Required(User)
+    character_name = Required(str)
+    user_message = Required(str)
+    character_message = Optional(str)
+    time = Optional(str)
+
+
+class Amplitude(db.Entity):
+    """ Имитация сервиса аналитики Amplitude """
+    user_id = Required(int)
+    act = Required(str)
+    time = Optional(str)
+
+
+class Welcome_text(db.Entity):
+    """ Приветственные сообщения персонажей """
+    character_name = Required(str)
+    text = Optional(str)
+
+
+db.bind(provider='sqlite', filename='bot_database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
+
+
+# ------ заполнение Welcome_text -------
+@db_session
+def write_in_welcome_text() -> None:
+    """ Заполнение приветствий """
+    text_einshteyn = 'Приветствую вас, уважаемый собеседник! Я - Альберт Эйнштейн, известный как тот парень, который любил физику,' \
+                     ' фантастическую укладку и загадки Вселенной. Можно сказать, я наделал черных дыр в науке, а мои мысли летали' \
+                     ' быстрее света (по крайней мере, в моей голове). Но сегодня я здесь, чтобы помочь вам с вашими вопросами и' \
+                     ' размышлениями. Как я могу быть полезен?'
+    text_mario = 'Привет! Я - Марио, известный как тот чувак в красной шапке.. Моя профессия? ' \
+                 'Спасать принцессу, конечно. И делать это в любой не понятной ситуации. Сегодня я здесь, чтобы добавить немного ' \
+                 'приключений и радуги в ваш день. Чем могу помочь, приятель?'
+
+    Welcome_text(character_name='Альберт Эйнштейн', text=text_einshteyn)
+    Welcome_text(character_name='Марио', text=text_mario)
+    db.commit()
+
+# заполнение
+@db_session
+def check_and_initialize_welcome_text():
+    if not Welcome_text.select(): write_in_welcome_text()
+
+check_and_initialize_welcome_text()
+# ------------------------------------------------
